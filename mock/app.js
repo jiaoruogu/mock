34cb52acc4b5ee071c5ecea2aaf793ea1e52862d
jiaoruogu <935065201@qq.com>
2022-09-30 09:22:27 +0800

    修改

--- mock/app.js
+++ mock/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 
+// post接收request body 的json数据
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 const port = process.env.PORT || 8848
 
 fs.readdirSync(`${__dirname}/router`).forEach(fileName => {
